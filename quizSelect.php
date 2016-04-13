4e6900c84f60860a4df864a9d224538ea642d82f
JGraziano2 <jgraziano2@gmail.com>
2016-04-13 12:17:06 -0600

    Added files via upload
    Changed radio inputs to select from database, added html and body tags.

--- quizSelect.php
+++ quizSelect.php
@@ -1,47 +1,45 @@
+<!DOCTYPE html>
+<html>
 <head>
-<?php require 'connection.php';?>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <?php require 'connection.php';?>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
-<div class="container">
-<h3>Select your quiz!</h3>
-<div class="panel panel-default">
-  <div class="panel-body">
-<form>
-<input type="radio" name="quizSelect" checked>General</input><br>
-<input type="radio" name="quizSelect">Networking</input><br>
-<input type="radio" name="quizSelect">Information Technology</input><br>
-<input type="radio" name="quizSelect">Software Development</input><br>
 
-<input type="text" name="studentEmail" value="email" autofocus=""><br>
+<body>
+    <div class="container">
+        <h3>Select your quiz!</h3>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <form>
+                <?php
+                    $sql = "SELECT quizName, 
+                                   quizID
+                            FROM quizzes";
 
+                    $result = mysqli_query($conn, $sql);
 
-<a href="preQuiz.html"><button type="button" class="btn btn-success">Go!</button></a>
-</form>
-<?php
-                        $sql = "SELECT Test_ID, 
-                                        Test_Name 
-                                FROM 
-                                        test";
-                        $result = mysqli_query($conn, $sql);
-
-                        if (mysqli_num_rows($result) > 0) {
-                            while($row = mysqli_fetch_assoc($result)) {
-                                echo "<p class='id'> id: " . $row["Test_ID"] . "</p>" . 
-                                    "Name: " . $row["Test_Name"] . 
-                                    "<br><br>";
-                            }
-                        } else {
-                            echo "0 results.";
+                    if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<input type='radio' name='quizSelect' value='" . $row[quizID] . "'>" . $row[quizName] . "</input><br>";
                         }
-                    ?>
+                    }else {
+                        echo "No results found.";
+                    }
+                ?>
 
-</div>
-</div>
-</div>
+                <input type="text" name="studentEmail" value="email" autofocus=""><br>
+
+                <a href="preQuiz.html"><button type="button" class="btn btn-success">Go!</button></a>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
