f2ad24d84aac222890a60d90de83f825ef1d57ad
dlowber <davidlowber@mycwi.cc>
2016-04-13 11:46:20 -0600

    Delete iaa_addUsers.sql

--- databaseScript/iaa_addUsers.sql
+++
@@ -1,9 +0,0 @@
-use iaa;
-
-insert into users
-Values ( null,'davidlowber', 'Pa$$w0rd', 'davidlowber@mycwi.cc', 2088503370, 'sa'),
-(null, 'jacobigraziano', 'Pa$$w0rd', 'jacobigraziano@mycwi.cc', 2084405378, 'sa'),
-(null, 'nikkivandermeer', 'Pa$$w0rd', 'nikkivandermeer@mycwi.cc', 2082833908, 'sa'),
-(null, 'austintruex', 'Pa$$w0rd', 'austintruex@mycwi.cc', 2085555555, 'sa');
-
-select * from users;
