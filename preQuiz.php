cded8b8d9bca58960a38d431754ef2696eb90b3a
NikkiVandermeer <NikkiVandermeer.WebDev@gmail.com>
2016-04-13 12:49:44 -0600

    Added files via upload

---
+++ preQuiz.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+<?php require 'connection.php'?>
+<meta charset="utf-8" />
+<title>Interest Assessment Pre-Quiz</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- user edit css -->
+<link rel="stylesheet" type="text/css" href="css/preQuiz.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+<body>
+	
+<div class="container">
+      <div class="jumbotron">
+      <img id="logo" src="images/Logo.png" />
+         
+     	</div>
+
+<div class="panel panel-default text-center">
+<div class="panel-heading" class="row text-center">
+
+ <div >
+    		  </div>
+</div>
+<div class="panel-body">
+<h2 class="text-center">This is NOT a test!</h2>
+<p>This is not a test determining whether you get into the program or not. This is simply a mannar of helping us determine where your current skills are placed, to help us better understand your current skill level.  Click below to begin the quiz!</p>
+<a href="quiz.html"><button type="button" class="btn btn-default">Begin Quiz</button></a>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
