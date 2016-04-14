2016a73958d80620a987776b6f0e7496970c4d57
NikkiVandermeer <NikkiVandermeer.WebDev@gmail.com>
2016-04-13 22:45:56 -0600

    Update and rename quizEdit.html to quizEdit.php

--- quizEdit.php
+++ quizEdit.php
@@ -1,16 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+<?php require 'connection.php'?>
+<meta charset="utf-8" />
+<title>Interest Assessment Quiz Edit</title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
+<!-- user edit css -->
+<link rel="stylesheet" type="text/css" href="css/quizEdit.css">
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<body>
+<div class="container">
+  <div class="jumbotron">
+  <img id="logo" src="images/Logo.png" />   
+    <h3>Edit your quiz</h3>
+    <div class="row text-right">
+    <div class="col-lg-12">
+      <p>Admin Name Here</p><span><a href="login.html"><button type="button" class="btn btn-default">Log Out</button></a></span></p>
+    </div>
+    </div>
+</div>
 
-<h3>Edit your quiz</h3>
 
-<a href="admin.html"><button type="button" class="btn btn-success">Go Back</button></a>
-<a href="addQuestion.html"><button type="button" class="btn btn-success">Add Question</button></a>
+<a href="admin.html"><button type="button" class="btn btn-default">Go Back</button></a>
+<a href="addQuestion.html"><button type="button" class="btn btn-default">Add Question</button></a>
 
 
 <div class="container">
@@ -29,7 +46,7 @@
                 <input type="text" name="q1a" value="Camels">
                 <br>
                 <br>
-                <button type="button" class="btn btn-primary">Update</button>
+                <button type="button" class="btn btn-default">Update</button>
             </form>
             <h4>Question 2</h4>
             <p><input type="text" name="q2" value="What is blah blah of the bloborian root of blah?"></p>
@@ -43,10 +60,11 @@
                 <input type="text" name="q2a" value="Camels">
                 <br>
                 <br>
-                <button type="button" class="btn btn-primary">Update</button>
+                <button type="button" class="btn btn-default">Update</button>
             </form>
           
         </div>
     </div>
 </div>
-
+</body>
+</html>
