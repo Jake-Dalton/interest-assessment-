93c2d424ab07e23634be741450d603a21a9010a1
JGraziano2 <jgraziano2@gmail.com>
2016-04-14 12:26:02 -0600

    Added files via upload
    Light formatting on prequiz.php

--- preQuiz.php
+++ preQuiz.php
@@ -1,55 +1,55 @@
 <!doctype html>
 <html lang="en">
-<head>
-<?php require 'connection.php';?>
-<meta charset="utf-8" />
-<title>Interest Assessment Pre-Quiz</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<!-- user edit css -->
-<link rel="stylesheet" type="text/css" href="css/preQuiz.css">
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <head>
+        <?php require 'connection.php';?>
+        <meta charset="utf-8" />
+        <title>Interest Assessment Pre-Quiz</title>
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <!-- user edit css -->
+        <link rel="stylesheet" type="text/css" href="css/preQuiz.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-</head>
-<body>
-	
-<div class="container">
-   <div class="jumbotron">
-     <img id="logo" src="images/Logo.png" />
-    </div>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    </head>
+    
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <img id="logo" src="images/Logo.png" />
+            </div>
 
-	<div class="panel panel-default text-center">
-	<div class="panel-heading" class="row text-center">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading" class="row text-center">
 
-      <div >
-   </div>
-</div>
-<div class="panel-body">
-<h2 class="text-center">This is NOT a test!</h2>
-<p>This is not a test determining whether you get into the program or not. This is simply a mannar of helping us determine where your current skills are placed, to help us better understand your current skill level.  Click below to begin the quiz!</p>
+                    <div>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <h2 class="text-center">This is NOT a test!</h2>
+                    <p>This is not a test determining whether you get into the program or not. This is simply a mannar of helping us determine where your current skills are placed, to help us better understand your current skill level.  Click below to begin the quiz!</p>
 
-<form action="quiz.php" method="post">
-<?php
-                    $sql = "SELECT quizName, 
+                    <form action="quiz.php" method="post">
+                        <?php
+                        $sql = "SELECT quizName, 
                                    quizID
                             FROM quizzes";
-                    $result = mysqli_query($conn, $sql);
-                    if (mysqli_num_rows($result) > 0) {
-                        while($row = mysqli_fetch_assoc($result)) {
-                            echo "<input type='radio' name='quizSelect' value='" . $row[quizID] . "'>" . $row[quizName] . "</input><br>";
+                        $result = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($result) > 0) {
+                            while($row = mysqli_fetch_assoc($result)) {
+                                echo "<input type='radio' name='quizSelect' value='" . $row[quizID] . "'>" . $row[quizName] . "</input><br>";
+                            }
+                        }else {
+                            echo "No results found.";
                         }
-                    }else {
-                        echo "No results found.";
-                    }
-                ?>
-                  <input type="submit" value="Submit">
-</form>
-</div>
-</div>
-</div>
-</div>
-</body>
+                        ?>
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
