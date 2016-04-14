3eea4cc1fbaf2eec0d51d665ac4a2ee4f4c484c4
NikkiVandermeer <NikkiVandermeer.WebDev@gmail.com>
2016-04-14 11:28:56 -0600

    Added files via upload

--- quizSelect.php
+++ quizSelect.php
@@ -7,22 +7,29 @@
 
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
+   <!-- custom styles -->
+   <link rel="stylesheet" type="text/css" href="css/quizSelect.css">
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div class="container">
-        <h3>Select your quiz!</h3>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <form>
+       <div class="container">
+         <div class="jumbotron">
+           <img id="logo" src="images/Logo.png" />	
+           <h3>Select Your Quiz</h3>
+       </div>
+       
+       <div class="panel panel-default">
+          <div class="panel-body">
+             <form>
                 <?php
                     $sql = "SELECT quizName, 
                                    quizID
                             FROM quizzes";
+
                     $result = mysqli_query($conn, $sql);
+
                     if (mysqli_num_rows($result) > 0) {
                         while($row = mysqli_fetch_assoc($result)) {
                             echo "<input type='radio' name='quizSelect' value='" . $row[quizID] . "'>" . $row[quizName] . "</input><br>";
@@ -32,12 +39,9 @@
                     }
                 ?>
 
-                <input type="text" name="studentEmail" value="email" required autofocus=""><br>
-                <input type="text" name="studentFName" value="first name"><br>
-                 <input type="text" name="studentLName" value="last name"><br>
-                <input type="text" name="studentEmail" value="studentID"><br>
+                <input type="text" name="studentEmail" value="email" autofocus><br>
 
-                <a href="preQuiz.html"><button type="button" class="btn btn-success">Go!</button></a>
+                <a href="preQuiz.php"><button type="button" class="btn btn-default">Go!</button></a>
                 </form>
             </div>
         </div>
