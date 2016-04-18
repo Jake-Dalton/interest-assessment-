835da13d235217f99cb24b898784201096c38c22
JGraziano2 <jgraziano2@gmail.com>
2016-04-18 13:21:15 -0600

    Added files via upload

--- admin.php
+++ admin.php
@@ -1,95 +1,94 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <?php require 'connection.php';?>
+    <head>
+        <?php require 'connection.php';?>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-    <!-- admin css -->
-    <link rel="stylesheet" type="text/css" href="css/admin.css">
+        <!-- admin css -->
+        <link rel="stylesheet" type="text/css" href="css/admin.css">
 
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-</head>
-    
-<body>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    </head>
 
-<?php 
-// this is to display the instructor info and their quiz
+    <body>
+
+        <?php 
+        // this is to display the instructor info and their quiz
         $emailSubmitted = htmlspecialchars($_POST['instructorEmail']);
-        
-        $sql = "SELECT instructors.instructorID, instructors.instructorEmail, quizzes.quizID, quizzes.quizName, instructors.instructorFirst, instructors.instructorLast FROM instructors JOIN quizzes WHERE instructors.instructorID = quizzes.instructorID AND instructors.instructorEmail = '" . $emailSubmitted."'";
+
+        $sql = "SELECT instructors.instructorID, instructors.instructorEmail, quizzes.quizID, quizzes.quizName, instructors.instructorFirst, instructors.instructorLast 
+                FROM instructors JOIN quizzes 
+                WHERE instructors.instructorID = quizzes.instructorID AND instructors.instructorEmail = '" . $emailSubmitted."'";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) > 0) {
-                            while($row = mysqli_fetch_assoc($result)) {
-                                $quizID = $row['quizID'];
-                                $quizName = $row['quizName'];
-                                $instructorName = $row['instructorFirst']. " " .$row['instructorLast'];
-                            }
-                        } else {
-                            echo "no results found";
-                            echo "<br> ".$emailSubmitted;
-                        }
-?>
+            while($row = mysqli_fetch_assoc($result)) {
+                $quizID = $row['quizID'];
+                $quizName = $row['quizName'];
+                $instructorName = $row['instructorFirst']. " " .$row['instructorLast'];
+            }
+        } else {
+            echo "no results found";
+            echo "<br> ".$emailSubmitted;
+        }
+        ?>
 
-
-
-    <div class="container">
-        <div class="jumbotron">
-            <img id="logo" src="images/Logo.png" />	
-            <h3>Admin Panel</h3>
-            <div class="row text-right">
-                <div class="col-lg-12">
-                    <p><?php echo $instructorName; ?></p><span><a href="login.html"><button type="button" class="btn btn-default">Log Out</button></a></span></p>
+        <div class="container">
+            <div class="jumbotron">
+                <img id="logo" src="images/Logo.png" />	
+                <h3>Admin Panel</h3>
+                <div class="row text-right">
+                    <div class="col-lg-12">
+                        <p><?php echo $instructorName; ?></p><span><a href="login.php"><button type="button" class="btn btn-default">Log Out</button></a></span></p>
                 </div>
             </div>
         </div>
 
         <div id="quizResults">
             <h4>Quiz Results</h4>
-            <table class="table table-bordered">
-                <tr>
-                    <th>Student First Name</th>
-                    <th>Student Last Name</th>
-                    <th>Student Email</th>
-                    <th>Student CWI ID</th>
-                    <th>Quiz Taken</th>
-                    <th>Score</th>
-                </tr>
-                <?php
-    $sql = "SELECT * FROM responses WHERE quizID = ".$quizID;
-    $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) > 0) {
-                            while($row = mysqli_fetch_assoc($result)) {
-                             echo "<tr> 
+            <table class="table table-hover table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>Student First Name</th>
+                        <th>Student Last Name</th>
+                        <th>Student Email</th>
+                        <th>Student CWI ID</th>
+                        <th>Quiz Taken</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $sql = "SELECT * 
+                        FROM responses 
+                        WHERE quizID = " . $quizID ."
+                        ORDER BY submitTime DESC";
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr> 
                                 <td>".$row['studentFirst']."</td>
                                 <td>".$row['studentLast']."</td>
                                 <td>".$row['studentEmail']."</td>
                                 <td>".$row['studentCwiID']."</td>
                                 <td>".$row['submitTime']."</td>
-                                <td></td>
+                                <td>".$row['studentScore']."</td>
+                                <td>
+                                    <button type='button' class='btn btn-default'>View</button>
+                                    <button type='button' class='btn btn-default'>Delete</button>
+                                </td>
                              </tr>";
-                            }
-                        } else {
-                            echo "no results found";
                         }
-?>
-                <tr>
-                    <td>Data</td>
-                    <td>Data</td>
-                    <td>Data</td>
-                    <td><?php echo $quizName ?></td>
-                    <td>Data</td>
-                    <td>Data</td>
-                <td>
-                    <button type="button" class="btn btn-default">View</button>
-                    <button type="button" class="btn btn-default">Delete</button>
-                </td>
-                </tr>
+                    } else {
+                        echo "no results found";
+                    }
+                    ?>
+                </tbody>
             </table>
         </div> <!-- quizResults -->
 
@@ -98,14 +97,46 @@
                 <h4><?php echo $quizName ?></h4>
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        YOUR QUIZ NAME & SOME DETAILS?
+                        <?php
+    $i = 1;
+                    $sql = "SELECT questionContent, questionID
+                                    FROM questions
+                                    WHERE quizID = " . $quizID;
+                    $result = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            $sql2 = "SELECT answerContent, answerID
+                                     FROM answers
+                                     WHERE questionID = " . $row[questionID];
+                            $result2 = mysqli_query($conn, $sql2);
+                            echo "<h4>Question " . $i . "</h4>
+                                          <p>" . $row[questionContent] . "</p>";
+                            if (mysqli_num_rows($result2) > 0) {
+                                while($row = mysqli_fetch_assoc($result2)) {
+                                    echo "<input type='radio' 
+                                                            name='answer" . $i . "' 
+                                                            value='" . $row[answerID] . "'
+                                                            >" . 
+                                        $row[answerContent] . 
+                                        "<br>";
+                                }
+                            } else {
+                                echo "No results found.";
+                            }
+                            ++$i;
+                        }
+                    } else {
+                        echo "No results found.";
+                    }
+                        ?>
                     </div>
                 </div>
                 <a href="quizEdit.php"><button type="button" class="btn btn-default">Edit Quiz</button></a>
             </div>
         </div><!--Your Quiz Row -->
-    </div> <!-- container -->
-</body>
+        </div> <!-- container -->
+    </body>
 </html>
 
 
