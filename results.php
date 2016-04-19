553916928ffacd62bc8acc06475efcd79427bbf8
NikkiVandermeer <NikkiVandermeer.WebDev@gmail.com>
2016-04-19 11:15:33 -0600

    Update results.php

--- results.php
+++ results.php
@@ -1,35 +1,37 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <?php require 'connection.php';?>
+  <head>
+    <?php require 'connection.php';?>
 
-        <meta charset="utf-8" />
-        <title>Interest Assessment Results</title>
+    <meta charset="utf-8" />
+    <title>Interest Assessment Results</title>
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
-        <!-- user edit css -->
-        <link rel="stylesheet" type="text/css" href="css/results.css" media="screen" />
-        <link rel="stylesheet" type="text/css" href="css/resultsPrint.css" media="print"/>
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    </head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+    <!-- user edit css -->
+    <link rel="stylesheet" type="text/css" href="css/results.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/resultsPrint.css" media="print"/>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </head>
 
-    <body>
-        <div class="container">
-            <div class="jumbotron">
-                <img id="logo" src="images/Logo.png" />   
-                <div class="row text-center">
-                    <h3>Results</h3>
-                    <div class="col-lg-12">
-                        <button type='button' class='btn btn-default'><a href="index.html">Home</a></button>
-                    </div>
-                </div>
-            </div>
+  <body>
+    <div class="container">
+      <div class="jumbotron">
+        <img id="logo" src="images/Logo.png" />
+        <div class="row text-center">
+          <h3>Results</h3>
+          <div class="col-lg-12">
+            <button type='button' class='btn btn-default'>
+              <a href="index.html">Home</a>
+            </button>
+          </div>
+        </div>
+      </div>
 
-            <?php
+      <?php
             $i = 1;
             $selectedQuizID = intval($_POST['selectedQuizID']);
             $numOfQuestions = intval($_POST['numOfQuestions']);
@@ -91,15 +93,15 @@
 
             ?>
 
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <?php
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <?php
                             echo "<h4>Test results for " . $fName . " " . $lName . "</h4>";
                             ?>
-                            <ul>
-                                <?php
+              <ul>
+                <?php
                                 $sql = "SELECT quizName
                                         FROM quizzes
                                         WHERE quizID = " . $selectedQuizID;
@@ -114,27 +116,27 @@
                                     echo "No results found.";
                                 }
                                 ?>
-                                <?php echo "<li>Your score was: " . $percentCorrect . "%</li>"; ?>
-                                <?php echo "<li>Placeholder Bullet for Box formatting</li>"; ?>
-                            </ul>
-                            <div class="text-right">
-                                <button onclick="printThis()" type="button" class="btn btn-default">Print Now</button>
-                                <script>
-                                    function printThis() {
-                                        window.print();
-                                    }
-                                </script>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <h4>Instructor Info:</h4>
-                            <ul>
-                                <!-- Need to update this SELECT statement to join with quizzes rather than just use the quizID as the instructorID -->
-                                <?php
+                <?php echo "<li>Your score was: " . $percentCorrect . "%</li>"; ?>
+                <?php echo "<li>Placeholder Bullet for Box formatting</li>"; ?>
+              </ul>
+              <div class="text-right">
+                <button onclick="printThis()" type="button" class="btn btn-default">Print Now</button>
+                <script>
+                  function printThis() {
+                  window.print();
+                  }
+                </script>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <h4>Instructor Info:</h4>
+              <ul>
+                <!-- Need to update this SELECT statement to join with quizzes rather than just use the quizID as the instructorID -->
+                <?php
                                 $sql = "SELECT instructors.instructorPhone, instructors.instructorEmail, instructors.instructorFirst, instructors.instructorLast 
                                         FROM instructors JOIN quizzes 
                                         WHERE quizzes.instructorID = instructors.instructorID AND quizzes.quizID =" . $selectedQuizID;
@@ -151,12 +153,14 @@
                                     echo "No results found.";
                                 }
                                 ?>
-                            </ul>
-                            <div class="text-right">
-                                <button type='button' class='btn btn-default'><a href='mailto:<?php
+              </ul>
+              <div class="text-right">
+                <button type="button" class="btn btn-default">
+                  <a href='mailto:'
+                    <?php
                                     $sql = "SELECT instructorEmail
-                                        FROM instructors JOIN quizzes 
-                                        WHERE quizzes.instructorID = instructors.instructorID AND quizzes.quizID =" . $selectedQuizID;
+                                        FROM instructors
+                                        WHERE instructorID = " . $selectedQuizID;
 
                                     $result = mysqli_query($conn, $sql);
                                     if (mysqli_num_rows($result) > 0) {
@@ -164,16 +168,19 @@
                                             echo $row[instructorEmail];
                                         }
                                     }
-                                    ?>?Subject=Test%20Results'>Email Now</a></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                                    ?>?Subject=Test%20Results'>Email Now
+                  </a>
+                </button>
+              </div>
             </div>
-        </div> <!-- closes container -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- closes container -->
 
-        <!-- Insert information into the database -->
-        <?php
+    <!-- Insert information into the database -->
+    <?php
 
         $quizID = intval($_POST['selectedQuizID']);
         $studentEmail = htmlspecialchars($_POST['studentEmail']);
@@ -190,5 +197,5 @@
         $result = mysqli_query($conn, $sql);
 
         ?>
-    </body>
+  </body>
 </html>
